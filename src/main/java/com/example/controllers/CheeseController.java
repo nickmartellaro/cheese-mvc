f4ecfeec3edc3b9ec09b84407a2a0f7b9cdb8f75
Nick Martellaro <nick.martellaro@gmail.com>
2017-03-14 17:12:08 -0500

    finished prep work

--- src/main/java/com/example/controllers/CheeseController.java
+++ src/main/java/com/example/controllers/CheeseController.java
@@ -62,4 +62,4 @@ public class CheeseController {
         // Redirect to /cheese
         return "redirect:";
     }
-    
+
---
+++ src/main/java/com/example/models/Cheese.java
@@ -0,0 +1,7 @@
+package com.example.models;
+
+/**
+ * Created by Nick Martellaro on 3/12/2017.
+ */
+public class Cheese {
+}
